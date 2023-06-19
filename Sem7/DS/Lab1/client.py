7e79004482f3411da55e41f4d01126d2e682724d
Bigyan <134137654+bigyanse@users.noreply.github.com>
2023-06-19 08:46:28 +0545

    `update`: client and server

--- Sem7/DS/Lab1/client.py
+++ Sem7/DS/Lab1/client.py
@@ -13,7 +13,23 @@ while True:
         param1 = input("Enter your name")
         param2 = input("Enter your address:")
         result = client.show(param1, param2)
+        print(f"result {result}")
+    elif operation == "login":
+        email = input("Email: ")
+        password = input("Password: ")
+        status = client.login(email, password)
+        if status == "success":
+            print("logging in")
+        else:
+            print(status)
+    elif operation == "register":
+        email = input("Email: ")
+        password = input("Password: ")
+        status = client.register(email, password)
+        if status == "success":
+            print("registering user")
+        else:
+            print(status)
     else:
         print("Invalid operation")
         continue
-    print(f"result: {result}")
--- Sem7/DS/Lab1/server.py
+++ Sem7/DS/Lab1/server.py
@@ -1,9 +1,28 @@
 from xmlrpc.server import SimpleXMLRPCServer
 
+users = {
+        "admin": "12345"
+}
+
 class display:
     def show(self, name, roll):
         detail = f"My name is {name} and roll number is {roll}"
         return detail
+
+    def register(self, email, password):
+        if email in users:
+            return "already registered"
+        users[email] = password
+        return "success"
+
+    def login(self, email, password):
+        if email in users:
+            if users[email] == password:
+                return "success"
+            else:
+                return "invalid email or password"
+        else:
+            return "invalid email or password"
 
 
 server = SimpleXMLRPCServer(("localhost", 8000))

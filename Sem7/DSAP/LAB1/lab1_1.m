798a8df6da6cfab58630f93675703c17cd5c16a9
Bigyan Dahal <134137654+bigyanse@users.noreply.github.com>
2023-06-26 08:10:42 +0545

    Add files via upload

---
+++ Sem7/DSAP/LAB1/lab1_1.m
@@ -0,0 +1,11 @@
+clc;
+close all;
+a = 5;
+f = 3;
+t = 0:0.01:1;
+x = a * sin(2 * pi* f * t);
+plot(t, x);
+xlabel('time');
+ylabel('amplitude');
+title('Continuous Sine/Bigyan Dahal/ACE076BCT025');
+grid on;
